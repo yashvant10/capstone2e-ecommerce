8908ca8acde38ea4a549d91311ef95475dd6bac1
Yashvant <yashvant10@github.com>
2026-07-11 11:37:39 +0530

    Update Node version to 22 for Supabase WebSocket support

---
+++ backend/test_server.js
@@ -0,0 +1,7 @@
+process.env.SUPABASE_URL = '';
+process.env.SUPABASE_ANON_KEY = '';
+process.env.DATABASE_URL = '';
+process.env.RAZORPAY_KEY_ID = '';
+process.env.RAZORPAY_KEY_SECRET = '';
+
+require('./dist/server.js');
